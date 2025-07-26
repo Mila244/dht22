6fd51dda1ef98b18a3ee49b67a1826497762e7d2
Mila244 <milaaguirre293@gmail.com>
2025-07-26 15:54:09 -0500

    subiendo 4

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "✅ Flask funcionando correctamente en Render 🎉"
+datos_sensor = []  # Para guardar los datos recibidos
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"mensaje": "Backend Flask funcionando 🚀", "datos": datos_sensor})
+
+@app.route('/sensor', methods=['POST'])
+def recibir_datos():
+    data = request.get_json()
+    datos_sensor.append(data)
+    print(f"📥 Datos recibidos: {data}")
+    return jsonify({"mensaje": "Datos recibidos correctamente"}), 200
 
 if __name__ == '__main__':
-    # Render asigna el puerto automáticamente a través de la variable de entorno PORT
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
